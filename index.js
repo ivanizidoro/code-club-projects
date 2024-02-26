21e9434ac6327eb920d174c4cd39982e8132cbd0
ivanizidoro <ivanizidoro@hotmail.com>
2024-02-26 19:44:36 -0300

    alteração-no-index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const uuid = require('uuid')
 
@@ -67,76 +66,4 @@ app.delete('/users/:id', checkUerId, (request, response) => {
 })
 
 app.listen(port, () => {
-    console.log(`🚀 Servidor rodando na porta ${port}`)
-=======
-const express = require('express')
-const uuid = require('uuid')
-
-const port = 3000
-const app = express()
-app.use(express.json())
-
-
-const users = []
-
-const checkUerId = (request, response, next) => {
-    const { id } = request.params
-
-    const index = users.findIndex(user => user.id === id)
-
-    if (index < 0) {
-        return response.status(404).json({ message: "User not found" })
-    }
-
-    request.userIndex = index
-
-    request.userId = id
-
-    next()
-}
-
-
-app.get('/users', (request, response) => {
-
-    return response.json(users)
-})
-
-app.post('/users', (request, response) => {
-
-    const { name, age } = request.body
-
-    const user = { id: uuid.v4(), name, age }
-
-    users.push(user)
-
-    return response.status(201).json(user)
-})
-
-app.put('/users/:id', checkUerId, (request, response) => {
-
-    const { name, age } = request.body
-
-    const index = request.userIndex
-
-    const id = request.userId
-
-    const updateUser = { id, name, age }
-
-    users[index] = updateUser
-
-    return response.json(updateUser)
-})
-
-app.delete('/users/:id', checkUerId, (request, response) => {
-
-    const index = request.userIndex
-
-    users.splice(index, 1)
-
-    return response.status(204).json()
-})
-
-app.listen(port, () => {
-    console.log(`🚀 Servidor rodando na porta ${port}`)
->>>>>>> 17f146833381a1dfb87f44f803df0baa2d79c984
-})
+    console.log(`🚀 Servidor rodando na porta ${port}`)})
